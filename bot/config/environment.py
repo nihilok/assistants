d4ce6c62b69c03739d5e0fddb7aa7381bf065453
Michael Jarvis <mjarvis@ep.com>
2024-08-12 00:40:20 +0100

    Add logging
    - adds `loguru` dependency
    - pins other dependencies
    - adds `bot.log` module
    - adds `bot.exceptions` module and outlines exception hierarchy
    - adds error handling and logging to stdout when `OPENAI_API_KEY` environment variable is missing

--- bot/config/environment.py
+++ bot/config/environment.py
@@ -1,3 +1,15 @@
 import os
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+from bot.exceptions import ConfigError
+from bot.log import logger
+
+try:
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+except KeyError as e:
+    error = "Missing required OPENAI_API_KEY environment variable"
+    logger.error(error)
+    raise ConfigError(error) from e
+
+ASSISTANT_INSTRUCTIONS = os.environ.get(
+    "ASSISTANT_INSTRUCTIONS", "You are a helpful assistant."
+)
---
+++ bot/exceptions.py
@@ -0,0 +1,6 @@
+class BotException(Exception):
+    pass
+
+
+class ConfigError(BotException):
+    pass
