d1480a1df143df97afd139d1bfa65a6a204541ee
Michael Jarvis <mjarvis@ep.com>
2024-08-12 01:08:17 +0100

    Mitigate infinite loop by checking last message id and raising an exception if it is the same on two consecutive iterations.

--- bot/cli.py
+++ bot/cli.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from bot.ai.assistant import Assistant
+from bot.exceptions import NoResponseError
 
 
 def cli():
@@ -8,17 +9,20 @@ def cli():
         "default-assistant", "gpt-4o-mini", "You are a helpful assistant."
     )
     thread_id = None
+    last_message_id = None
     while (user_input := input(">>> ")).lower() not in {"q", "quit"}:
         if thread_id is None:
             run, message = asyncio.run(assistant.prompt(user_input))
             thread_id = run.thread_id
         else:
             run, message = asyncio.run(assistant.prompt(user_input, thread_id))
-        print(
-            assistant.client.beta.threads.messages.list(
-                thread_id=run.thread_id, order="asc"
-            )
-            .data[-1]
-            .content[0]
-            .text.value
-        )
+
+        messages = assistant.client.beta.threads.messages.list(
+            thread_id=run.thread_id, order="asc"
+        ).data
+
+        if messages[-1].id == last_message_id:
+            raise NoResponseError
+
+        last_message_id = messages[-1].id
+        print(messages[-1].content[0].text.value)
