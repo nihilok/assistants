45636207377a320f886618da6a0b67ee3dc362f3
Michael Jarvis <mjarvis@ep.com>
2024-08-15 13:12:41 +0100

    Adds more features to CLI front end
    - ability to continue the previous thread with option `-t` (previous thread id is saved in a file in the user's home dir: .assistant-last-thread-id)
    - ability to prepopulate the editor with text provided as positional args
    - ability to provide instructions via option `-i` which override the default/environment variable instructions
    - ability to copy previous response from the bot prompt/command line using the command `-c`, or to copy only the codeblock examples from the response using `-cb`

--- bot/cli.py
+++ bot/cli.py
@@ -1,4 +1,9 @@
+import argparse
+import os
+import re
 import sys
+
+import pyperclip
 
 from bot.ai.assistant import Assistant
 from bot.config.environment import DEFAULT_MODEL, ASSISTANT_INSTRUCTIONS
@@ -18,60 +23,165 @@ class TerminalColours:
     UNDERLINE = "\033[4m"
 
 
+PERSISTENT_THREAD_ID_FILE = f"{os.environ.get('HOME', '.')}/.assistant-last-thread-id"
+
+
 def cli():
     user_input = ""
+    initial_input = ""
+    last_message = None
 
-    opts = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="CLI for AI Assistant")
 
-    if "-e" in opts:
-        initial_input = get_text_from_default_editor()
-    elif opts:
-        initial_input = " ".join(opts)
-    else:
-        initial_input = ""
+    parser.add_argument(
+        "-e", action="store_true", help="Open the default editor to compose a prompt."
+    )
+    parser.add_argument(
+        "-f",
+        metavar="INPUT_FILE",
+        type=str,
+        help="Read the initial prompt from a file (e.g., 'input.txt').",
+    )
+    parser.add_argument(
+        "-s",
+        metavar="INSTRUCTIONS",
+        type=str,
+        help="Read the initial system message / instructions from a specified file "
+        "(if not provided, defaults will be used).",
+    )
+    parser.add_argument(
+        "-t",
+        action="store_true",
+        help="Continue previous thread.",
+    )
+    parser.add_argument(
+        "positional_args",
+        nargs="*",
+        help="Positional arguments to concatenate into a single prompt. E.g. ./cli.py This is a single prompt.",
+    )
 
+    args = parser.parse_args()
+
+    instructions = args.s if args.s else ASSISTANT_INSTRUCTIONS
+
+    thread_id = None
+    if args.t:
+        try:
+            with open(PERSISTENT_THREAD_ID_FILE, "r") as f:
+                thread_id = f.read().strip("\n")
+        except FileNotFoundError:
+            print(
+                "Warning: could not read last thread id from ~/.assistant-last-thread-id - starting new thread..."
+            )
+
+    if args.e:
+        # Open the default editor to compose formatted prompt
+        if args.positional_args:
+            # If text was provided as positional args alongside the `-e` option, concatenate to a single string and
+            # we can pre-populate the editor (or rather the temp file used by the editor) with this text.
+            initial_cli_input = " ".join(args.positional_args)
+        else:
+            initial_cli_input = None
+        initial_input = get_text_from_default_editor(initial_cli_input)
+    elif args.f:
+        # Read the initial prompt from a file
+        try:
+            with open(args.f, "r") as file:
+                initial_input = file.read()
+        except FileNotFoundError:
+            print(f"Error: The file '{args.f}' was not found.")
+            sys.exit(1)
+    elif args.positional_args:
+        initial_input = " ".join(args.positional_args)
+
+    # Create the assistant
     assistant = Assistant(
         "AI Assistant",
         DEFAULT_MODEL,
-        ASSISTANT_INSTRUCTIONS,
+        instructions,
         tools=[{"type": "code_interpreter"}],
     )
-    last_message = None
+
+    # IO Loop
     try:
         while initial_input or (
             user_input := input(TerminalColours.OKGREEN + ">>> ")
-        ).lower() not in {
-            "q",
-            "quit",
-        }:
+        ).lower() not in {"q", "quit", "exit"}:
             if initial_input:
-                print(initial_input)
+                print(TerminalColours.OKGREEN + ">>> " + initial_input)
+                user_input = initial_input
 
-            elif not user_input:
+            if not user_input.strip():
                 continue
 
-            elif user_input.strip() == "-e":
-                user_input = get_text_from_default_editor()
+            else:
+                user_input = user_input.strip()
 
-                if not user_input:
+            if user_input.lower() == "-e":
+                user_input = get_text_from_default_editor()
+                print(user_input)
+            elif user_input.lower() == "-c":
+                if not last_message:
+                    print(
+                        TerminalColours.WARNING
+                        + "No previous message to copy from."
+                        + TerminalColours.ENDC
+                    )
+                    continue
+                previous_response = last_message.content[0].text.value
+                pyperclip.copy(previous_response)
+                print(TerminalColours.OKBLUE + "Copied response to clipboard")
+                continue
+            elif user_input.lower() == "-cb":
+                if not last_message:
+                    print(
+                        TerminalColours.WARNING
+                        + "No previous message to copy from."
+                        + TerminalColours.ENDC
+                    )
+                    continue
+                previous_response = last_message.content[0].text.value
+                code_blocks = re.split(
+                    r"(```.*?```)", previous_response, flags=re.DOTALL
+                )
+                code_only = [
+                    "\n".join(block.split("\n")[1:-1]).strip()
+                    for block in code_blocks
+                    if block.startswith("```")
+                ]
+
+                if not code_only:
+                    print(
+                        TerminalColours.WARNING
+                        + "No codeblocks in previous message."
+                        + TerminalColours.ENDC
+                    )
                     continue
 
-                print(user_input)
+                all_code = "\n\n".join(code_only)
+                pyperclip.copy(all_code)
+                print(TerminalColours.OKBLUE + "Copied code blocks to clipboard")
+                continue
 
             print(TerminalColours.ENDC, end="")
-
             message = assistant.converse(
                 initial_input or user_input,
-                last_message.thread_id if last_message else None,
+                last_message.thread_id if last_message else thread_id,
             )
 
-            initial_input = ""  # Only relevant for first iteration (comes from initial command line)
+            initial_input = ""  # Only relevant for first iteration (comes from initial command line),
+            # resetting to empty string here, so it won't be evaluated as truthy in future iterations
 
             if last_message and message.id == last_message.id:
                 raise NoResponseError
 
-            last_message = message
             print(message.content[0].text.value, end="\n\n")
+            last_message = message
+
+            if not thread_id:
+                with open(PERSISTENT_THREAD_ID_FILE, "w") as file:
+                    file.write(last_message.thread_id)
 
     except (EOFError, KeyboardInterrupt):
+        # Exit gracefully if ctrl+C or ctrl+D are pressed
         sys.exit(0)
--- bot/helpers.py
+++ bot/helpers.py
@@ -3,10 +3,14 @@ import subprocess
 import tempfile
 
 
-def get_text_from_default_editor():
+def get_text_from_default_editor(initial_text=None):
     # Create a temporary file
     with tempfile.NamedTemporaryFile(suffix=".md", delete=False) as temp_file:
         temp_file_path = temp_file.name
+
+    if initial_text:
+        with open(temp_file_path, "w") as text_file:
+            text_file.write(initial_text)
 
     # Open the editor for the user to input text
     editor = os.environ.get("EDITOR", "nano")
