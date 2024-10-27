34f97119a211037ee874ba4014676341eb67a01e
Michael Jarvis <mjarvis@ep.com>
2024-10-27 17:14:03 +0000

    Change order of setting env vars

--- setup_dbs.py
+++ setup_dbs.py
@@ -1,11 +1,11 @@
 import asyncio
 import os
 
-from bot.telegram_ui.sqlite_user_data import SqliteUserData
-from bot.user_data.sqlite_backend import init_db
-
 os.environ.setdefault("TG_USER_DATA", "tg_user_data.db")
 os.environ.setdefault("USER_DATA_DB", "assistant_data.db")
+
+from bot.telegram_ui.sqlite_user_data import SqliteUserData
+from bot.user_data.sqlite_backend import init_db
 
 asyncio.run(init_db())
 user_data = SqliteUserData()
